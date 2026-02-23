efb0c1a07e68d2c78f470a11d7fdd84e6424b731
arj6nn <apk03061@gmail.com>
2026-02-23 16:24:42 +0530

    fixed docker-compose file and the URL in the passport file

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://54.83.117.62:7777/auth/google/callback"
+            callbackURL: process.env.GOOGLE_CALLBACK_URL
         },
 
         async (accessToken, refreshToken, profile, done) => {
